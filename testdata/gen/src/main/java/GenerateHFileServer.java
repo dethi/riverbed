b86bdcc1325d3e20e9175ce9a517af1f97e21163
Thibault Deutsch <thibault.deutsch@gmail.com>
2026-02-16 18:19:52 -0800

    testdata/gen: cleanup unused mode

--- testdata/gen/src/main/java/GenerateHFileServer.java
+++ testdata/gen/src/main/java/GenerateHFileServer.java
@@ -47,15 +47,7 @@ public class GenerateHFileServer {
         String compression = "NONE";
         String dataBlockEncoding = "NONE";
         String bloomType = "NONE";
-        boolean includeTags = false;
         int blockSize = 65536;
-        int cellCount = 10;
-        String[] families = {"cf"};
-        String[] qualifiers = {"q"};
-        int valueSize = 6;
-        long timestamp = 1700000000000L;
-
-        // Recipe mode fields.
         long seed;
         String family;
         RowGroup[] groups;
@@ -75,11 +67,7 @@ public class GenerateHFileServer {
 
             try {
                 Config cfg = gson.fromJson(line, Config.class);
-                if (cfg.groups != null) {
-                    generateHFileFromRecipe(conf, fs, cfg);
-                } else {
-                    generateHFile(conf, fs, cfg);
-                }
+                generateHFileFromRecipe(conf, fs, cfg);
                 System.out.println(cfg.outputPath);
                 System.out.flush();
             } catch (Exception e) {
@@ -89,79 +77,6 @@ public class GenerateHFileServer {
             }
         }
     }
-
-    private static void generateHFile(Configuration conf, FileSystem fs, Config cfg) throws IOException {
-        Path path = new Path(cfg.outputPath);
-
-        // TODO: consider adding the following lines to switch between zstd-jni and hadoop-native
-        // conf.set("hbase.io.compress.zstd.codec", "org.apache.hadoop.hbase.io.compress.zstd.ZstdCodec");
-        // Compression.Algorithm.ZSTD.reload(conf);
-
-        HFileContext context = new HFileContextBuilder()
-                .withCompression(Compression.Algorithm.valueOf(cfg.compression))
-                .withDataBlockEncoding(DataBlockEncoding.valueOf(cfg.dataBlockEncoding))
-                .withIncludesTags(cfg.includeTags)
-                .withBlockSize(cfg.blockSize)
-                .build();
-
-        BloomType bloom = BloomType.valueOf(cfg.bloomType);
-        long maxKeyCount = (long) cfg.cellCount * cfg.families.length * cfg.qualifiers.length;
-
-        StoreFileWriter writer = new StoreFileWriter.Builder(conf, fs)
-                .withFilePath(path)
-                .withFileContext(context)
-                .withBloomType(bloom)
-                .withMaxKeyCount(maxKeyCount)
-                .build();
-
-        // Sort families and qualifiers to ensure HBase key order.
-        String[] families = cfg.families.clone();
-        String[] qualifiers = cfg.qualifiers.clone();
-        Arrays.sort(families);
-        Arrays.sort(qualifiers);
-
-        // Generate cells in HBase sort order: row, family, qualifier.
-        // For each row, iterate families then qualifiers.
-        for (int i = 0; i < cfg.cellCount; i++) {
-            for (String family : families) {
-                for (String qualifier : qualifiers) {
-                    byte[] row = Bytes.toBytes(String.format("row-%05d", i));
-                    byte[] fam = Bytes.toBytes(family);
-                    byte[] qual = Bytes.toBytes(qualifier);
-                    byte[] value = generateValue(i, cfg.valueSize);
-
-                    KeyValue kv;
-                    if (cfg.includeTags) {
-                        List<Tag> tags = new ArrayList<>();
-                        // Tag type 1 (TTL_TAG_TYPE) with a simple value.
-                        tags.add(new ArrayBackedTag((byte) 1, Bytes.toBytes(String.format("tag-%05d", i))));
-                        kv = new KeyValue(row, fam, qual, cfg.timestamp, value, tags);
-                    } else {
-                        kv = new KeyValue(row, fam, qual, cfg.timestamp, value);
-                    }
-                    writer.append(kv);
-                }
-            }
-        }
-
-        writer.close();
-    }
-
-    private static byte[] generateValue(int index, int valueSize) {
-        if (valueSize == 0) {
-            return new byte[0];
-        }
-        // Zero-pad the index to fill valueSize bytes.
-        String s = String.format("%0" + valueSize + "d", index);
-        // If the formatted string is longer than valueSize (shouldn't happen for reasonable sizes),
-        // take the last valueSize characters.
-        if (s.length() > valueSize) {
-            s = s.substring(s.length() - valueSize);
-        }
-        return Bytes.toBytes(s);
-    }
-
-    // ---- Recipe mode ----
 
     private static MessageDigest sha256() {
         try {
@@ -272,6 +187,10 @@ public class GenerateHFileServer {
             }
             if (hasTags) break;
         }
+
+        // TODO: consider adding the following lines to switch between zstd-jni and hadoop-native
+        // conf.set("hbase.io.compress.zstd.codec", "org.apache.hadoop.hbase.io.compress.zstd.ZstdCodec");
+        // Compression.Algorithm.ZSTD.reload(conf);
 
         HFileContext context = new HFileContextBuilder()
                 .withCompression(Compression.Algorithm.valueOf(cfg.compression))
